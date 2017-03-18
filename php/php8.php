dab2c55daf4b9bc95f67c098aa48bf002e52500a
shadiashafi <shadia.shafi@gmail.com>
2017-03-19 01:51:38 +0530

    php

---
+++ php/php8.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+define('MIN_VALUE', '0.0');   
+define('MAX_VALUE', '1.0');   
+
+//const MIN_VALUE = 2.0;         //redeclaration error
+//const MAX_VALUE = 3.0;        //redeclaration error
+echo MIN_VALUE."<br>";
+echo MAX_VALUE;
+
+
+ ?>
---
+++ php/php9.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = "";
+
+switch ($x) {
+    case "6b":
+        echo "I’m 6 b";
+        break;
+    case "6bz":
+        echo "I’m 6az";
+        break;
+    case "6":
+        echo "I’m integer 6";
+        break;
+        case 6:
+        echo "integer 6";
+        break;
+    default:
+        echo "I’m no where";
+}
+?>
+ 
+</body>
+</html>
