1c9f5e4e64a65b6b5c918ec3fa259dfa1aedd91a
shadiashafi <shadia.shafi@gmail.com>
2017-04-04 16:31:38 +0530

    php

---
+++ php/php15.php
@@ -0,0 +1,11 @@
+<?php 
+
+$english_word = array('consider'=>"deem to be",'minute'=>"immeasureably small",'accord'=>"concurrence of opinion");
+print_r($english_word);
+echo "<br>";
+print_r($english_word['consider']);
+echo "<br>";
+print_r($english_word['minute']);
+echo "<br>";
+print_r($english_word['accord']);
+?>
